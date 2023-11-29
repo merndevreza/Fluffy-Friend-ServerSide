9e7a93af5c586d93b8a74951ebd4812bbd01ad74
merndevreza <merndevreza@gmail.com>
2023-11-29 10:02:38 +0600

    API created for pet details and requist for adoption

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app=express()
-const port=process.env.PORT||5000;
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //====================
 //Mongo DB
@@ -22,68 +21,89 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     await client.connect();
-     const slidesCollection = client.db("fluffyFriends").collection("slides");
-     const categoriesCollection = client.db("fluffyFriends").collection("categories");
-     const reviewsCollection = client.db("fluffyFriends").collection("reviews");
-     const petsCollection = client.db("fluffyFriends").collection("pets");
-     const donationsCollection = client.db("fluffyFriends").collection("donations");
+    await client.connect();
+    const slidesCollection = client.db("fluffyFriends").collection("slides");
+    const categoriesCollection = client
+      .db("fluffyFriends")
+      .collection("categories");
+    const reviewsCollection = client.db("fluffyFriends").collection("reviews");
+    const petsCollection = client.db("fluffyFriends").collection("pets");
+    const donationsCollection = client
+      .db("fluffyFriends")
+      .collection("donations");
+    const adoptionsCollection = client
+      .db("fluffyFriends")
+      .collection("adoption-requests");
 
-     // slides
-     app.get("/slides",async(req,res)=>{
-      const result=await slidesCollection.find().toArray();
-      res.send(result)
-     })
-     //categories
-     app.get("/categories",async(req,res)=>{
-      const result=await categoriesCollection.find().toArray();
-      res.send(result)
-     })
-     //reviews
-     app.get("/reviews",async(req,res)=>{
-      const result=await reviewsCollection.find().toArray();
-      res.send(result)
-     })
-     //pets
-     app.get("/pets",async(req,res)=>{
-      const result=await petsCollection.find().toArray();
-      res.send(result)
-     })
-     //donations
-     app.get("/donations",async(req,res)=>{
-      const result=await donationsCollection.find().toArray();
-      res.send(result)
-     })
-     //donation details
-     app.get("/donation/details/:id",async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result= await donationsCollection.findOne(query)
-      res.send(result)
-     })
+    // slides
+    app.get("/slides", async (req, res) => {
+      const result = await slidesCollection.find().toArray();
+      res.send(result);
+    });
+    //categories
+    app.get("/categories", async (req, res) => {
+      const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    });
+    //reviews
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+    //pets
+    app.get("/pets", async (req, res) => {
+      const result = await petsCollection.find().toArray();
+      res.send(result);
+    });
+    //pet details
+    app.get("/pet/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petsCollection.findOne(query);
+      res.send(result);
+    });
+    //Pet Adoption Requests
+    app.post("/adoption-requests", async (req, res) => {
+      const request = req.body;
+      const result = await adoptionsCollection.insertOne(request);
+      res.send(result);
+    });
 
-
+    //donations
+    app.get("/donations", async (req, res) => {
+      const result = await donationsCollection.find().toArray();
+      res.send(result);
+    });
+    //donation details
+    app.get("/donation/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationsCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-   //  await client.close();
+    //  await client.close();
   }
 }
 run().catch(console.dir);
 
-//Express 
-app.get("/",(req,res)=>{
-   res.send("Fluffy Friends Server is running")
-})
-app.listen(port,()=>{
-console.log(`Fluffy Friend Server is running on port on ${port}`);
-})
+//Express
+app.get("/", (req, res) => {
+  res.send("Fluffy Friends Server is running");
+});
+app.listen(port, () => {
+  console.log(`Fluffy Friend Server is running on port on ${port}`);
+});
